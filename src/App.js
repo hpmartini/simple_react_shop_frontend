049073fa315c7be25e3e6815a908ecb6239cd592
hape <hapemartini@gmail.com>
2021-12-08 18:51:33 +0100

    add rudimentary navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Product from "./pages/Product";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <Home/>;
+  return <Navbar />;
 };
 
 export default App;
