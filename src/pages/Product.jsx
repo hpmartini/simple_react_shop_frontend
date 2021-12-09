d9a5bf4e609eb384d3767b0a4706075aa081bf35
hape <hapemartini@gmail.com>
2021-12-09 14:58:35 +0100

    add product details page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,169 @@
+import * as React from 'react';
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
+import {Add, Remove} from "@material-ui/icons";
+
+const Container = styled.div`
+
+`;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Description = styled.p`
+  margin: 20px 0;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  Width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${({color}) => (color)};
+  margin: 0 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option`
+
+`;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+    return (
+        <Container>
+            <Announcement/>
+            <Navbar/>
+            <Wrapper>
+                <ImageContainer>
+                    <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
+                </ImageContainer>
+                <InfoContainer>
+                    <Title>Nutrixs cadunt!</Title>
+                    <Description>
+                        Aye there's nothing like the dead malaria singing on the pegleg.
+                        Per guest prepare one container of triple sec with scraped steak for dessert.
+                        With peanut butter drink whipped cream. Arg! Pieces o' fortune are forever fine.
+                        It is a conscious sensor, sir.
+                    </Description>
+                    <Price>42 €</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black"/>
+                            <FilterColor color="darkblue"/>
+                            <FilterColor color="gray"/>
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove/>
+                            <Amount>1</Amount>
+                            <Add/>
+                        </AmountContainer>
+                        <Button>ADD TO CART</Button>
+                    </AddContainer>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter/>
+            <Footer/>
+        </Container>
+    );
+};
+
+export default Product;
