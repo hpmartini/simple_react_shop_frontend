eb17ce190951870879d4494fe5cae9ddb8c5407f
hape <hapemartini@gmail.com>
2021-12-09 10:28:41 +0100

    add Products

--- src/data.js
+++ src/data.js
@@ -67,7 +67,7 @@ export const popularProducts = [
     },
     {
         id:7,
-        img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+        img:"https://www.vintageindustries.nl/download_front/ahkxoi1467/_overview_large_/2060_Darren_parka_Oak_2_.png",
     },
     {
         id:8,
