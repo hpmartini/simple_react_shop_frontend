3736cbe4863454761d0cfbccf2081f513a2fe515
hape <hapemartini@gmail.com>
2021-12-18 18:51:27 +0100

    populate sizes with API data

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -168,11 +168,9 @@ const Product = () => {
                         <Filter>
                             <FilterTitle>Size</FilterTitle>
                             <FilterSize>
-                                <FilterSizeOption>XS</FilterSizeOption>
-                                <FilterSizeOption>S</FilterSizeOption>
-                                <FilterSizeOption>M</FilterSizeOption>
-                                <FilterSizeOption>L</FilterSizeOption>
-                                <FilterSizeOption>XL</FilterSizeOption>
+                                {product.size?.map((size) => (
+                                    <FilterSizeOption key={size}>{size}</FilterSizeOption>
+                                ))}
                             </FilterSize>
                         </Filter>
                     </FilterContainer>
