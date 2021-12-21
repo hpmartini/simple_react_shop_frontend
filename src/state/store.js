640b1199384bb880cd0e6394303dc5df6cb3694a
hape <hapemartini@gmail.com>
2021-12-21 17:07:20 +0100

    add userStore to the global store

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,10 @@
  import { configureStore } from '@reduxjs/toolkit'
  import cartStore from './cartStore'
+ import userStore from './userStore'
 
  export default configureStore({
      reducer: {
          cart: cartStore,
+         user: userStore,
      }
  })
