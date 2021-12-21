bee31875cb131f0cefa10c19c16f99b282bb2bf5
hape <hapemartini@gmail.com>
2021-12-21 20:13:51 +0100

    persist store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import store from './state/store'
+import {store, persistor} from './state/store'
+import {PersistGate} from "redux-persist/integration/react";
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,37 @@
  import { configureStore } from '@reduxjs/toolkit'
+ import {
+     persistStore,
+     persistReducer,
+     FLUSH,
+     REHYDRATE,
+     PAUSE,
+     PERSIST,
+     PURGE,
+     REGISTER,
+ } from 'redux-persist'
  import cartStore from './cartStore'
  import userStore from './userStore'
+ import storage from 'redux-persist/lib/storage'
 
- export default configureStore({
+ const persistConfig = {
+    key: 'root',
+     version: 1,
+     storage
+ }
+
+ const persistedReducer = persistReducer(persistConfig, userStore)
+
+ export const store = configureStore({
      reducer: {
          cart: cartStore,
-         user: userStore,
-     }
+         user: persistedReducer,
+     },
+     middleware: (getDefaultMiddleware) =>
+         getDefaultMiddleware({
+             serializableCheck: {
+                 ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+             },
+         }),
  })
+
+ export const persistor = persistStore(store)
